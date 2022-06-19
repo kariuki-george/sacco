3b828d35d99dd8ec7cac19d13da500b2ddb2be33
Kariuki George <kariukigeorge2030@gmail.com>
2022-06-19 19:38:25 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //app.enableCors();
   //app.use(helmet());
+  app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+  next();
+});
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
